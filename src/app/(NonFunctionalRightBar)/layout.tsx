52dc4f6e56f7afda5dbf5fd47451fc6edf2a7365
Dhruv Solanki <95128593+dhruvsolanki0811@users.noreply.github.com>
2024-04-05 14:41:15 +0530

    feat: Set up NextAuth.js with Prisma adapter and session storage for user details
    This commit implements authentication using NextAuth.js with the Prisma adapter for database interactions and session storage for storing user details.
    
    Changes Made:
    - Integrated NextAuth.js with Prisma adapter for authentication.
    - Configured session storage to store user details securely.
    - Updated environment variables for authentication secrets and database connection.
    - Added necessary API routes and components for authentication flow.

---
+++ src/app/(NonFunctionalRightBar)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NonFunctionRightBarLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
+
+export default NonFunctionRightBarLayout;
---
+++ src/app/(auth)/api/auth/[...nextauth]/option.ts
@@ -0,0 +1,89 @@
+import type { NextAuthOptions, User } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/lib/db";
+import type { Adapter } from "next-auth/adapters";
+import { compare } from "bcrypt";
+
+export const options: NextAuthOptions = {
+  adapter: PrismaAdapter(db) as Adapter,
+  pages: {
+    signIn: "/signin",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "Email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
+        },
+        type: {
+          label: "User Type",
+          type: "text",
+          placeholder: "Type of User",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+        let user;
+        if (credentials.type == "Organization") {
+          user = await db.organization.findUnique({
+            where: { email: credentials?.email },
+          });
+        } else if (credentials.type == "Jobseeker") {
+          user = await db.jobSeeker.findUnique({
+            where: { email: credentials?.email },
+          });
+        }
+        if (!user) {
+          return null;
+        }
+        const passwordMatch = await compare(
+          credentials.password,
+          user.password
+        );
+        if (!passwordMatch) {
+          return null;
+        }
+
+        return {
+          id: `${user.id}`,
+          username: user.username,
+          email: user.email,
+          role: credentials.type,
+        };
+      },
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.username = user.username;
+        token.role = user.role;
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) {
+        session.user.id = token.id;
+        session.user.username = token.username;
+        session.user.role = token.role;
+      }
+      return session;
+    },
+  },
+};
